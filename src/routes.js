4c08fe326c373d7bcafdc14820330b99a5b0a1e3
leandro.carra <leandro.carra@luizalabs.com>
2020-07-07 18:24:47 -0300

    add migrations and db

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'aioo silllver'})
-})
+  return res.json({ message: 'aioo silllver' });
+});
 
 export default routes;
