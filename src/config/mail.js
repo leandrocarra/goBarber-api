d699ffbe5447a7f0cad25d7d14b273dcb5fee462
leandro.carra <leandro.carra@luizalabs.com>
2020-08-25 18:19:00 -0300

    template email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '40d68bb7d23837',
+    pass: '79e1a58817bf5f',
+  },
+  default: {
+    from: 'Leleco <semreplay@barbudos.com>',
+  },
+};
